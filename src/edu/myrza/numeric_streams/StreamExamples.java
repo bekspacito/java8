9d88f736623177bcca1507a000e68bbf7af654e2
Myrza <Gapusta97@gmail.com>
2018-10-03 16:56:40 +0600

    Wrote pythagorian triple generator

--- src/edu/myrza/numeric_streams/StreamExamples.java
+++ src/edu/myrza/numeric_streams/StreamExamples.java
@@ -6,11 +6,12 @@ import static edu.myrza.util.DishCollectionCreator.*;
 import java.util.List;
 import java.util.OptionalDouble;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamExamples {
     public static void main(String[] args){
 
-        exampleThreeInvoker();
+        exampleThree();
 
     }
 
@@ -40,75 +41,21 @@ public class StreamExamples {
     }
 
 
-    private static void exampleThreeInvoker(){
+    private static void exampleThree(){
 
-        exampleThree(7,24);
+     int aUpperBound = 100;
+     int bUpperBound = 100;
 
-    }
+     Stream<int[]> res = IntStream.range(1,aUpperBound)
+                                     .boxed()
+                                     .flatMap(a -> IntStream.range(a,bUpperBound)
+                                                            .filter(b -> Math.sqrt(a*a + b*b) % 1 == 0)
+                                                            .mapToObj(b -> new int[]{a,b,(int)Math.sqrt(a*a + b*b)})
+                                     );
 
-    private static void exampleThree(int aUpperBound,int bUpperBond){
+      res.limit(5)
+         .forEach(a -> System.out.println("[" + a[0] + "," + a[1] + "," + a[2] + "]"));
 
-        IntStream.rangeClosed(1,aUpperBound)
-                          .boxed()
-                          .flatMap(a -> IntStream.rangeClosed(1,bUpperBond)
-                                                .filter(b -> {
-                                                     double c = Math.sqrt(a*a + b*b);
-                                                     return !(c > Math.floor(c));
-                                                 })
-                                                .boxed()
-                                                .map(b -> new PythTriple(a,b,Math.sqrt(a*a + b*b)))
-                         )
-                         .distinct()
-                         .forEach(System.out::println);
-
-
-    }
-
-    private static class PythTriple {
-
-        public final double a;
-        public final double b;
-        public final double c;
-
-        public PythTriple(double a, double b, double c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            PythTriple that = (PythTriple) o;
-
-            if (Double.compare(that.a, a) != 0) return false;
-            if (Double.compare(that.b, b) != 0) return false;
-            return Double.compare(that.c, c) == 0;
-        }
-
-        @Override
-        public int hashCode() {
-            int result;
-            long temp;
-            temp = Double.doubleToLongBits(a);
-            result = (int) (temp ^ (temp >>> 32));
-            temp = Double.doubleToLongBits(b);
-            result = 31 * result + (int) (temp ^ (temp >>> 32));
-            temp = Double.doubleToLongBits(c);
-            result = 31 * result + (int) (temp ^ (temp >>> 32));
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            return "PythTriple{" +
-                    "a=" + a +
-                    ", b=" + b +
-                    ", c=" + c +
-                    '}';
-        }
     }
 
 }
