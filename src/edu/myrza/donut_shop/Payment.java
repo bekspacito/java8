1e625cb4df63d32798a8e460e26c543131856b77
Myrza <Gapusta97@gmail.com>
2018-11-14 21:24:38 +0600

    Added groupByCard method into Payment class.You can find description inside Payment class

--- src/edu/myrza/donut_shop/Payment.java
+++ src/edu/myrza/donut_shop/Payment.java
@@ -1,5 +1,10 @@
 package edu.myrza.donut_shop;
 
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.*;
+
 public class Payment{
 
     public final CreditCard card;
@@ -16,6 +21,21 @@ public class Payment{
             return new Payment(card,this.amount + payment.amount);
         else
             throw new  IllegalStateException("Cards dont match...");
+    }
+
+    /**
+     * @param payments represent a list of mixed payments for example :
+     * [pc1 pc2 pc2 pc1 pc1 pc3 pc4 pc2 pc1 pc1] where pc(n)- payment of credit card number (n)
+     * method groupByCard() groups them by card : [pc1 pc1 pc1 pc1 pc1] [pc2 pc2 pc2] [pc3] [pc4]
+     * and combines them
+     * */
+    public static List<Payment> groupByCard(List<Payment> payments){
+        return payments.stream()
+                       .collect(groupingBy(p -> p.card))
+                       .values()
+                       .stream()
+                       .map(list -> list.stream().reduce(Payment::combine).get())
+                       .collect(toList());
     }
 
 }
--- src/edu/myrza/donut_shop/Tester.java
+++ src/edu/myrza/donut_shop/Tester.java
@@ -1,14 +1,55 @@
 package edu.myrza.donut_shop;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Tester{
     public static void main(String[] args){
+
+        test2();
+
+    }
+
+    public static void test1(){
 
         CreditCard card = new CreditCard(1000);
         Tuple<List<Donut>,Payment> purchase = DonutShop.buyDonuts(5,card);
         System.out.println(new Donut().price*5 == purchase._2.amount);
         System.out.println(card.equals(purchase._2.card));
 
+    }
+
+    public static void test2(){
+
+        CreditCard card1 = new CreditCard(1000);
+        CreditCard card2 = new CreditCard(700);
+        CreditCard card3 = new CreditCard(7000);
+
+        List<Tuple<Payment,Donut>> purchases = new ArrayList<>();
+
+        //card1 = 3
+        //card2 = 3
+        //card3 = 3
+        purchases.add(DonutShop.buyDonut(card1));
+        purchases.add(DonutShop.buyDonut(card1));
+        purchases.add(DonutShop.buyDonut(card2));
+        purchases.add(DonutShop.buyDonut(card3));
+        purchases.add(DonutShop.buyDonut(card3));
+        purchases.add(DonutShop.buyDonut(card2));
+        purchases.add(DonutShop.buyDonut(card1));
+        purchases.add(DonutShop.buyDonut(card2));
+        purchases.add(DonutShop.buyDonut(card3));
+
+        List<Payment> mixedPayments = purchases.stream()
+                                                .map(p -> p._1)
+                                                .collect(Collectors.toList());
+
+        List<Payment> combinedByCardPayments = Payment.groupByCard(mixedPayments);
+
+        System.out.println(combinedByCardPayments.size() == 3);
+        System.out.println(combinedByCardPayments.get(0).amount == new Donut().price*3);
+        System.out.println(combinedByCardPayments.get(1).amount == new Donut().price*3);
+        System.out.println(combinedByCardPayments.get(2).amount == new Donut().price*3);
     }
 }
